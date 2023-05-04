d4a1fdce4580e839fee51cc42df525f1c1382534
Jurgita Gestautas <jnj0205@gmail.com>
2023-05-04 09:45:35 -0500

    first commit

---
+++ homework3.py
@@ -0,0 +1,56 @@
+#Exercise 1    
+#Given an array of positive integers nums, return a list of all of the negative integers.
+#Ex. 1
+#nums = [1, 3, 5, 7, 8]
+#Expected Output: [-1, -3, -5, -7, -8]
+
+
+def negative_numbers (nums):
+numbers = [1, 3, 5, 7, 8]
+for num in x:
+     x * (-1)
+print(negative_numbers)
+
+
+#Ex. 2
+#nums = [100, 534, 32, 15, 77, 222, 788, 345, 75645, 22]
+#Expected Output: [-100, -534, -32, -15, -77, -222, -788, -345, -75645, -22]#
+
+
+##Exercise 2#########
+###
+#Given a string, return a list of all of the digits in the string.
+#Ex. 1
+#address = "123 Real Street, Apt. 2, Springfield, OR 43498"
+#Expected Output: ['1', '2', '3', '2', '4', '3', '4', '9', '8']
+
+#Ex. 2
+#sentence = "My phone number is (555) 555-4321"
+#Expected Output: ['5', '5', '5', '5', '5', '5', '4', '3', '2', '1']
+#
+def only_number(number):
+        if number == digit:
+            print(number)
+    
+        else:
+             skip
+
+print(number)
+        
+       
+ ###
+  ##Given a string digits, return a string of the digits + 1
+
+##Ex. 1
+#digits = '123'
+#Expected Output: '124'
+
+#Ex. 2
+#digits = '99'
+#Expected Output: '100'
+
+def f(x):
+    return x + 1
+
+y = f(99)
+print(y)
---
+++ test.py
@@ -0,0 +1,8 @@
+
+address = "123 Real Street, Apt. 2, Springfield, OR 43498"
+def numbers (n):
+    i = address
+    for i in range(0,9):
+        return i
+
+print(numbers)
